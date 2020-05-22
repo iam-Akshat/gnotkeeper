201ae6e43dfee182a1fe9e9dcefe51402af309bf
Akshat <masterakshata@gmail.com>
2020-05-22 08:56:27 +0530

    Added footer,header and note componenets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.jsx'
+import App from './App.jsx';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-   
+  <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
