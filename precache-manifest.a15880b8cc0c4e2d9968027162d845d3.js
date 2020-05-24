16698364037f0328ab1eea86051f780d0183ed4c
Akshat <masterakshata@gmail.com>
2020-05-24 19:21:28 +0530

    Updates

---
+++ precache-manifest.a15880b8cc0c4e2d9968027162d845d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95dcca76b52a3b25b2c4b544ab37202",
+    "url": "/gnotkeeper/index.html"
+  },
+  {
+    "revision": "a6c585e3416b5fbb0889",
+    "url": "/gnotkeeper/static/css/main.d14b29dd.chunk.css"
+  },
+  {
+    "revision": "6ea268422dfb9fd54649",
+    "url": "/gnotkeeper/static/js/2.081e77ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gnotkeeper/static/js/2.081e77ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c585e3416b5fbb0889",
+    "url": "/gnotkeeper/static/js/main.edd0e164.chunk.js"
+  },
+  {
+    "revision": "8e3e30a83bb8965c3837",
+    "url": "/gnotkeeper/static/js/runtime-main.ad52d43c.js"
+  }
+]);
